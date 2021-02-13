b5cdfbe27d5cbf5325250768cf912f14f6c3efd4
viveklele <viveklele123@gmail.com>
2021-02-13 10:27:53 +0530

    my first web project

---
+++ nebula/web/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+#class myAppNameConfig(AppConfig):
+#    name = 'myAppName'
+	#verbose_name = 'A Much Better Name'
+
+	
+class WebConfig(AppConfig):
+    name = 'web'
+	
